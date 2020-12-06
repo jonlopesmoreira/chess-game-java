86000040b16c278f4f7cb33adace69c898894d32
jonlopesmoreira <jonprojetos@hotmail.com>
2020-12-06 15:47:52 -0300

    git ignore

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ import boardgame.Position;
 public class Program {
 
 	public static void main(String[] args) {
-		Position p = new Position(1,2);
+		Position p = new Position(4,2);
 		System.out.println(p);
 
 	}
