5a13a7b7e7319a06987788c2e8cc58038fbbf8eb
jonlopesmoreira <jonprojetos@hotmail.com>
2020-12-07 13:25:18 -0300

    fix possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,6 +28,13 @@ public class ChessMatch
 		}
 		return matriz;
 	}
+
+	public boolean[][] possibleMoves(ChessPosition sourcePosition)
+	{
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
 	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition)
 	{
