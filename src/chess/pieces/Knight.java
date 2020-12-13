85cf76c08cfc350759dbdc7f12c7049c3e0d7a8d
jonlopesmoreira <jonprojetos@hotmail.com>
2020-12-13 17:24:30 -0300

    knight

---
+++ src/chess/pieces/Knight.java
@@ -0,0 +1,76 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Knight extends ChessPiece {
+
+	public Knight(Board board, Color color) {
+		super(board, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "N";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getCols()];
+		Position p = new Position(0, 0);
+
+
+		p.setValues(position.getRow() - 1, position.getCol()-2);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+
+		p.setValues(position.getRow() -2, position.getCol()-1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		p.setValues(position.getRow() -2, position.getCol() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		p.setValues(position.getRow()-1, position.getCol() + 2);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		p.setValues(position.getRow()+1, position.getCol() + 2);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		p.setValues(position.getRow()+2, position.getCol() +1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		p.setValues(position.getRow()+2, position.getCol() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		p.setValues(position.getRow()+1, position.getCol() - 2);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		return mat;
+		
+		
+		
+	}
+
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
+}
